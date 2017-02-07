0d635e0e6cf2b3a22524eb145012d1d116673c2c
Gene Chorba <gchorba@gmail.com>
2017-02-07 14:48:38 -0500

    13 toolbar

--- app/src/main/java/com/ionic/in/criminalintent/CrimeLab.java
+++ app/src/main/java/com/ionic/in/criminalintent/CrimeLab.java
@@ -14,12 +14,13 @@ public class CrimeLab
   private CrimeLab(Context context)
   {
     mCrimes = new ArrayList<>();
-    for (int i = 0; i < 100; i++) {
-      Crime crime = new Crime();
-      crime.setTitle("Crime #" + i);
-      crime.setSolved(i % 2 == 0); // Every other one
-      mCrimes.add(crime);
-    }
+    //goodbye random crimes
+//    for (int i = 0; i < 100; i++) {
+//      Crime crime = new Crime();
+//      crime.setTitle("Crime #" + i);
+//      crime.setSolved(i % 2 == 0); // Every other one
+//      mCrimes.add(crime);
+//    }
   }
 
   public static CrimeLab get(Context context)
@@ -29,9 +30,12 @@ public class CrimeLab
     }
     return sCrimeLab;
   }
-
+  public void addCrime(Crime c) {
+    mCrimes.add(c);
+  }
   public List<Crime> getCrimes()
   {
+
     return mCrimes;
   }
   public Crime getCrime(UUID id)
