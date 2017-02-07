604ef5a9210a5de9fecb7f81b36891fbc8b8da54
Gene Chorba <gchorba@gmail.com>
2017-02-07 13:28:52 -0500

    changes

---
+++ app/src/main/java/com/ionic/in/criminalintent/DatePickerFragment.java
@@ -0,0 +1,41 @@
+package com.ionic.in.criminalintent;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Date;
+
+/**
+ * Created by gene on 2/7/17.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
--- app/src/main/java/layout/CrimeFragment.java
+++ app/src/main/java/layout/CrimeFragment.java
@@ -4,6 +4,7 @@ package layout;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -15,8 +16,8 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.ionic.in.criminalintent.Crime;
-import com.ionic.in.criminalintent.CrimeActivity;
 import com.ionic.in.criminalintent.CrimeLab;
+import com.ionic.in.criminalintent.DatePickerFragment;
 import com.ionic.in.criminalintent.R;
 
 import java.util.UUID;
@@ -25,71 +26,78 @@ import java.util.UUID;
  * A simple {@link Fragment} subclass.
  */
 public class CrimeFragment extends Fragment {
-  private  static final String ARG_CRIME_ID = "crime_id";
-  private Crime mCrime;
-  private EditText mTitleField;
-  private Button mDateBtn;
-  private CheckBox mSolvedCheckBox;
+    private static final String ARG_CRIME_ID = "crime_id";
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateBtn;
+    private CheckBox mSolvedCheckBox;
+    private static final String DIALOG_DATE = "DialogDate";
 
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID, crimeId);
 
-  public static CrimeFragment newInstance(UUID crimeId){
-      Bundle args = new Bundle();
-      args.putSerializable(ARG_CRIME_ID, crimeId);
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
-      CrimeFragment fragment = new CrimeFragment();
-      fragment.setArguments(args);
-      return fragment;
-  }
-  public CrimeFragment() {
-  }
+    public CrimeFragment() {
+    }
 
-  @Override
-  public void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-      UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
-      mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
 
-  }
+    }
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState)
-  {
-    View v = inflater.inflate(R.layout.fragment_crime, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
-    mTitleField = (EditText) v.findViewById(R.id.crime_title);
-    mTitleField.setText(mCrime.getTitle());
-    mTitleField.addTextChangedListener(new TextWatcher() {
-      @Override
-      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        // Intentionally blank!
-      }
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Intentionally blank!
+            }
 
-      @Override
-      public void onTextChanged(CharSequence s, int start, int before, int count) {
-        mCrime.setTitle(s.toString());
-      }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
 
-      @Override
-      public void afterTextChanged(Editable s) {
-        // Intentionally blank!
-      }
-    });
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Intentionally blank!
+            }
+        });
 
-    mDateBtn = (Button) v.findViewById(R.id.crime_date);
-    mDateBtn.setText(mCrime.getDate().toString());
-    mDateBtn.setEnabled(false);
+        mDateBtn = (Button) v.findViewById(R.id.crime_date);
+        mDateBtn.setText(mCrime.getDate().toString());
+        mDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
-    mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
-    mSolvedCheckBox.setChecked(mCrime.isSolved());
-    mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-      @Override
-      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        mCrime.setSolved(isChecked);
-      }
-    });
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
 
-    return v;
-  }
+        return v;
+    }
 
 }
